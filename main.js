080f5f5eab9ee57b39a627efdacaf4d6626b9251
Dean-The-Dream <lawrence_dean@icloud.com>
2022-09-27 13:36:48 -0400

    complete score keeper

--- main.js
+++ main.js
@@ -1,47 +1,14 @@
 const { end } = ("@popperjs/core");
-const player1Score = document.querySelector(".p1");
-const player2Score = document.querySelector(".p2");
+let player1Score = document.querySelector(".p1");
+let player2Score = document.querySelector(".p2");
 const scoreBox = document.querySelector(".score-limit input");
-const maxScore = ((scoreBox.innerText) ? scoreBox.value = 2 : "5");
+let maxScore = 5;
+const reset = document.querySelector(".reset");
 
 
-function clickButton (e){
-    console.log("it works")
-    if((player1Score.innerText == maxScore)  || (player2Score.innerText == maxScore)){
-        endGame();
-    } else {
-
-        if (e.target.className.includes("p1Up")){
-            addPointP1();
-            console.log("the game continues")
-        }
-        if (e.target.className.includes("p2Up")){
-            addPointP2();
-        }
-        if (e.target.className.includes("reset")){
-            resetScreen()
-        }
-    }
-    
-}
-
-
-
-function addPointP1 (){
-    console.log("add a point")
-    player1Score = `${+player1Score.innerText++}`
-    if(+player1Score == maxScore){ endGame();}
-}
-
-function addPointP2 (){
-    player2Score = `${+player2Score.innerText++}`
-    if(+player2Score == maxScore){ endGame();}
-}
-function resetScreen (){
-
-}
 
 function endGame (){
+    console.log("end game")
     document.removeEventListener("click", clickButton)
     if(+player1Score.innerText > +player2Score.innerText){
         player1Score.style = "color: green"
@@ -50,7 +17,64 @@ function endGame (){
         player2Score.style = "color: green"
         player1Score.style = "color: red"
     }
+    document.querySelector(".p1Up").style = "color: rgba(0, 0, 0, .5)"
+    document.querySelector(".p2Up").style = "color: rgba(0, 0, 0, .5)"
 
 }
 
+function addPointP1 (){
+    console.log("add a point")
+    player1Score.innerText = `${(+player1Score.innerText) + 1}`;
+    console.log("test1")
+    if(+player1Score.innerText === maxScore){ 
+        console.log("test")
+    }
+    
+}
+
+function addPointP2 (){
+    player2Score.innerText = `${(+player2Score.innerText) + 1}`
+    if(+player2Score.innerText === maxScore){ endGame();}
+
+}
+
+function resetScreen (){
+        scoreBox.value="5";
+        maxScore = 5;
+        player1Score.innerText = 0;
+        player2Score.innerText = 0;
+        player1Score.style = null
+        player2Score.style = null
+        document.addEventListener("click",clickButton)
+        document.querySelector(".p1Up").style = null
+        document.querySelector(".p2Up").style = null
+}
+
+
+
+
+function clickButton (e){
+    if (e.target.className.includes("p1Up")){
+        addPointP1();
+        console.log("the game continues")
+    }
+    if (e.target.className.includes("p2Up")){
+        console.log("the game continues")
+        addPointP2();
+    }
+    if (e.target.className.includes("reset")){
+        resetScreen()
+    }
+     if((+player1Score.innerText === maxScore)  || (+player2Score.innerText === maxScore)){
+        endGame();
+    }
+    
+}
+
+
+
+
+
 document.addEventListener("click",clickButton)
+reset.addEventListener("click", resetScreen)
+scoreBox.addEventListener("blur",(e) => {maxScore = +scoreBox.value})
